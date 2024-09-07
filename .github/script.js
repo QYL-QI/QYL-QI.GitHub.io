50231ca5bbae188bc5510de9c1f00c9f24065fb0
QYL-QI <qiyanluo8@gmail.com>
2024-09-07 12:43:22 +0800

    Create script.js

---
+++ .github/script.js
@@ -0,0 +1,10 @@
+<body>
+  <h1>Hi, I'm Your Name!</h1>  
+  <p>Welcome to my website!</p>
+  
+  <!-- 添加一个按钮 -->
+  <button id="myButton">Click me!</button>
+  
+  <!-- 引入外部的 JavaScript 文件 -->
+  <script src="script.js"></script>
+</body>
